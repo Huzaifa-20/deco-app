0b227b9d0ab11d3324b6af8036cf2153260a1795
MoHuzaifa <muhammad.huzaifa@tintash.com>
2022-01-28 01:13:53 +0500

    Complete static intro section

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from './style/theme';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+export default theme;
---
+++ src/style/theme.tsx
@@ -0,0 +1,26 @@
+import { createTheme } from '@material-ui/core/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#F4F9FC',
+    },
+    secondary: {
+      main: '#000000',
+    },
+    text: {
+      primary: '#0F1B61',
+      secondary: '#FFFFFF',
+    },
+  },
+  typography: {
+    fontFamily: 'Montserrat',
+    fontWeightBold: '600',
+    h1: {
+      fontWeight: 400,
+    },
+  },
+});
+
+export const logoPrimaryColor = '#A8BAFA';
+export const logoSecondaryColor = '#DE9433';
